8208d9d06a9defece0bf6b15fbf6a3d07a515ff5
KingScaramanga <alexandre.ziaja@gmail.com>
2024-07-21 13:57:40 +0200

    - variabilisation du taux pour les produits de première necessite

--- src/app/shared/tax-calculator/premiereNecessiteStrategy.ts
+++ src/app/shared/tax-calculator/premiereNecessiteStrategy.ts
@@ -1,8 +1,10 @@
 import {TaxStrategy} from "./taxStrategy";
+import {arrondiTaxe} from "../functions/calculatePriceTTC";
+import {TAUX_PREMIERE_NECESSITE} from "../constants/taux-constants";
 
 export class PremiereNecessiteStrategy implements TaxStrategy{
   calculateTaxe(prixHt: number): number {
-    return prixHt;
+    return prixHt + arrondiTaxe(prixHt * TAUX_PREMIERE_NECESSITE / 100);
   }
 
 }
